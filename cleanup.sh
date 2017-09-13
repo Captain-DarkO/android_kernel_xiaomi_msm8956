03bafcee956f7cc3193db7769c47b3b507c48514
DeeJay-NuKe <ask222g@gmail.com>
2017-09-14 02:29:23 +0530

    update build setup

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,7 @@
 make clean && make mrproper
 echo "removing generated files"
 rm -rf ./arch/arm64/boot/dts/kenzo-msm8956-mtp.dtb
+rm -rf ./arch/arm64/boot/dt.img
 rm -rf ./buildtools/out/kernel
 rm -rf ./build/boot.img
 echo "cleaned up"
